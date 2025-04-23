74f59999b2c3a018140fad2aa7acf49bdba907e6
Hamidreza Jafari <hamidreza.j@outlook.com>
2025-04-22 22:46:01 -0400

    Add PerishableProductProperties subclass with expiration date and override toString

--- script.js
+++ script.js
@@ -17,3 +17,23 @@ class ProductProperties {
     )}, Quantity: ${this.quantity}`;
   }
 }
+
+// Subclass: PerishableProductProperties
+
+class PerishableProductProperties extends ProductProperties {
+  constructor(name, price, quantity, expirationDate) {
+    super(name, price, quantity);
+    this.expirationDate = expirationDate;
+  }
+
+  toString() {
+    return `${super.toString()}, Expiration Date: ${this.expirationDate}`;
+  }
+}
+
+// Sample instances
+const milk = new PerishableProductProperties('Milk', 1.5, 10, '2024-12-31');
+const eggs = new PerishableProductProperties('Eggs', 2.0, 12, '2024-11-15');
+
+console.log(milk.toString());
+console.log(eggs.toString());
